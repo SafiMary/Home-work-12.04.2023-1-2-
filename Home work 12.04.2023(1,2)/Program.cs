93b13f51417b8c534258049889358fa1a32fbb70
Мария Сафи <safim@yandex.ru>
2023-04-15 17:11:09 +0500

    Add project files.

---
+++ Home work 12.04.2023(1,2)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_12._04._2023_1_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 4-значное число: ");
+            string input = Console.ReadLine();
+            for (int i = input.Length- 1; i >= 0; i--)
+            {
+                Console.WriteLine(input[i]);
+            }
+            Console.ReadKey();
+
+            string input1;
+            int tmp;
+            Console.WriteLine("Введите 6-значное число: ");
+            input1 = Console.ReadLine();
+            try
+            {
+                for (int i = 0;i < input1.Length; i++)
+                {
+                    tmp = int.Parse(input1[i].ToString());
+                    if (tmp % 2 == 0)
+                    {
+                        Console.WriteLine(tmp);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неправильный ввод!!");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
